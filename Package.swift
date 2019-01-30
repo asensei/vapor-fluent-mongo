33abd7983891597ec600fb4ccc0ff6f77ebe836d
Valerio Mazzeo <valeriomazzeo@users.noreply.github.com>
2019-01-30 16:07:49 +0000

    mapIfDuplicatedKeyError (#5)

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
       .package(url: "https://github.com/vapor/core.git", .upToNextMajor(from: "3.5.0")),
       .package(url: "https://github.com/vapor/fluent.git", .upToNextMajor(from: "3.1.2")),
-      .package(url: "https://github.com/mongodb/mongo-swift-driver.git", .upToNextMajor(from: "0.0.8"))
+      .package(url: "https://github.com/mongodb/mongo-swift-driver.git", .upToNextMajor(from: "0.0.9"))
     ],
     targets: [
         .target(name: "FluentMongo", dependencies: ["Async", "Fluent", "MongoSwift"]),
