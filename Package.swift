df51bf5401d9c5d12302db27846b18299df0a0e3
Valerio Mazzeo <valerio.mazzeo@gmail.com>
2019-04-11 11:31:03 +0100

    dependencies update

--- Package.swift
+++ Package.swift
@@ -16,9 +16,9 @@ let package = Package(
       .library(name: "FluentMongo", targets: ["FluentMongo"])
     ],
     dependencies: [
-      .package(url: "https://github.com/vapor/core.git", .upToNextMajor(from: "3.5.0")),
-      .package(url: "https://github.com/vapor/fluent.git", .upToNextMajor(from: "3.1.2")),
-      .package(url: "https://github.com/mongodb/mongo-swift-driver.git", .upToNextMajor(from: "0.0.9"))
+      .package(url: "https://github.com/vapor/core.git", .upToNextMajor(from: "3.8.1")),
+      .package(url: "https://github.com/vapor/fluent.git", .upToNextMajor(from: "3.2.0")),
+      .package(url: "https://github.com/mongodb/mongo-swift-driver.git", .exact("0.0.9"))
     ],
     targets: [
         .target(name: "FluentMongo", dependencies: ["Async", "Fluent", "MongoSwift"]),
