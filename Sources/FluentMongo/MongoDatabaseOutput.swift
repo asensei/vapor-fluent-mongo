c881c8c4cdf2bb77e3038d8b8e5f2bbf64ca0ff8
Valerio Mazzeo <valerio.mazzeo@gmail.com>
2020-06-04 17:58:29 +0100

    allow for nested decoding of parent object

--- Sources/FluentMongo/MongoDatabaseOutput.swift
+++ Sources/FluentMongo/MongoDatabaseOutput.swift
@@ -28,25 +28,11 @@ private struct MongoDatabaseOutput: DatabaseOutput {
     }
 
     func schema(_ schema: String) -> DatabaseOutput {
-
-        let document: Document
-
-        switch (self.schema, schema) {
-        case (.some(let lhs), let rhs):
-            if lhs == rhs {
-                document = self.document
-            } else {
-                document = self.document[schema]?.documentValue ?? Document()
-            }
-        case (.none, _):
-            document = self.document
-        }
-
-        return MongoDatabaseOutput(document: document, decoder: self.decoder, schema: schema, fields: self.fields)
+        return MongoDatabaseOutput(document: self.document, decoder: self.decoder, schema: schema, fields: self.fields)
     }
 
     func contains(_ key: FieldKey) -> Bool {
-        guard !self.document.hasKey(key.mongoKey) else {
+        guard !self.namespace.hasKey(key.mongoKey) else {
             return true
         }
 
@@ -61,7 +47,7 @@ private struct MongoDatabaseOutput: DatabaseOutput {
     }
 
     func decodeNil(_ key: FieldKey) throws -> Bool {
-        switch self.document[key.mongoKey] {
+        switch self.namespace[key.mongoKey] {
         case .undefined, .null, .none:
             return true
         default:
@@ -70,7 +56,7 @@ private struct MongoDatabaseOutput: DatabaseOutput {
     }
 
     func decode<T>(_ key: FieldKey, as type: T.Type) throws -> T where T: Decodable {
-        guard self.document.hasKey(key.mongoKey) else {
+        guard self.namespace.hasKey(key.mongoKey) else {
             switch T.self {
             case is ExpressibleByNilLiteral.Type:
                 guard let result = (nil as Any?) as? T else {
@@ -83,6 +69,14 @@ private struct MongoDatabaseOutput: DatabaseOutput {
             }
         }
 
-        return try self.decoder.decode(type, from: self.document, forKey: key.mongoKey)
+        return try self.decoder.decode(type, from: self.namespace, forKey: key.mongoKey)
+    }
+
+    private var namespace: Document {
+        guard let schema = self.schema else {
+            return self.document
+        }
+
+        return self.document[schema]?.documentValue ?? self.document
     }
 }
