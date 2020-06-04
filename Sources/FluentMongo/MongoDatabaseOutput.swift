55f33f19833b38e0d3510555d315ded1de663c25
Valerio Mazzeo <valerio.mazzeo@gmail.com>
2020-06-04 15:14:16 +0100

    global namespace in nested schema

--- Sources/FluentMongo/MongoDatabaseOutput.swift
+++ Sources/FluentMongo/MongoDatabaseOutput.swift
@@ -27,33 +27,27 @@ private struct MongoDatabaseOutput: DatabaseOutput {
     }
 
     func schema(_ schema: String) -> DatabaseOutput {
-
-        let document: Document
-
-        switch (self.schema, schema) {
-        case (.some(let lhs), let rhs):
-            if lhs == rhs {
-                document = self.document
-            } else {
-                document = self.document[schema]?.documentValue ?? Document()
-            }
-        case (.none, _):
-            document = self.document
-        }
-
-        return MongoDatabaseOutput(document: document, decoder: self.decoder, schema: schema)
+        return MongoDatabaseOutput(document: self.document, decoder: self.decoder, schema: schema)
     }
 
     func contains(_ key: FieldKey) -> Bool {
-        return self.document[key.mongoKey] != nil
+        return self.namespace[key.mongoKey] != nil
     }
 
     func decodeNil(_ key: FieldKey) throws -> Bool {
-        return self.document[key.mongoKey] == .null
+        return self.namespace[key.mongoKey] == .null
     }
 
     func decode<T>(_ key: FieldKey, as type: T.Type) throws -> T where T: Decodable {
-        return try self.decoder.decode(type, from: self.document, forKey: key.mongoKey)
+        return try self.decoder.decode(type, from: self.namespace, forKey: key.mongoKey)
+    }
+
+    private var namespace: Document {
+        guard let schema = self.schema else {
+            return self.document
+        }
+
+        return self.document[schema]?.documentValue ?? self.document
     }
 }
 
